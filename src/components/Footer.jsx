7bc72c523ba2476597d44f4185189b44867bc09b
Yash Patel <yash3patel2005@gmail.com>
2025-07-13 13:03:37 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='w-screen bg-violet-300 py-4 text-black'>
+      
+        <p className='text-center text-sm'>
+          &copy; Nova 2024. All rights reserved
+        </p>
+
+    </footer>
+  )
+}
+
+export default Footer
